c2acb13c8f8d80e397ab2dd12c86161f09c37385
Anna <anavalentinasalram@gmail.com>
2026-02-27 17:09:52 -0600

    version 1.0.0

---
+++ practica_1/practica_1.py
@@ -0,0 +1,9 @@
+# Programa: Cálculo del área de un triángulo
+# Autor: Munuwa
+# Versión: 1.0.0 
+# Descripcion: Cálcula el área de un triángulo a partir de la base y la altura
+
+base = 10
+altura = 5
+area = base * altura
+print("el área del triángulo es:", area)    
