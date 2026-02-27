48b17b6fc8771740e3e21f646fb0934678316869
salvaxtraw5337 <epsuv5337@gmail.com>
2026-02-27 17:56:38 -0600

    practica_1.cpp

--- practica_1/practica_1.cpp
+++ practica_1/practica_1.cpp
@@ -1,16 +1,16 @@
 // Programa: Cálculo del área de un triángulo
 // Autor: axtraw8008
-// Versión: 1.1.0
+// Versión: 1.2.0
 // Descripcion: Cálcula el área de un triángulo a partir de la base y la altura
 
 #include <stdio.h>
 int main(){
-	int b, h, a;
-	printf("Ingrese la base: ");
+	float b, h, a;
+	printf("Ingrese la base en metros: ");
 	scanf("%f", &b);
-	printf("Ingrese el altura: ");
+	printf("Ingrese la altura en metros: ");
 	scanf("%f", &h);
-	a = (b * h)/2;
-	printf("El area es: %f", a);
+	a = (b * h)/2.0;
+	printf("El area es: %.3f metros", a);
 	return 0;
 }
