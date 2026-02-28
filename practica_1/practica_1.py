e56dfe11114f9c0fedba7a182ea8e0ec3685861b
Anna <anavalentinasalram@gmail.com>
2026-02-27 18:25:05 -0600

    version 2.0.0

--- practica_1/practica_1.py
+++ practica_1/practica_1.py
@@ -1,10 +1,20 @@
-# Programa: Cálculo del área de un triángulo
+# Programa: Cálculo del área de una figura
 # Autor: Munuwa
-# Versión: 1.1.0
-# Descripcion: Cálcula el área de un triángulo a partir de la base y la altura
+# Versión: 2.0.0
+# Descripcion: Cálcula el área de una figura dependiendo el dato solicitado
 
-base = input("Ingrese la base: ")
-altura = input("Ingrese el área: ")
-area = (base * altura)/2
+print("Seleccione la figura:")
+print("1. Triangulo")
+print("2. Cuadrado")
+opcion = int(input("Ingrese el numero de la opcion: "))
 
-print("El área del triángulo es:", area)    
+if opcion == 1:
+    base = int(input("Ingrese la base: "))
+    altura = int(input("Ingrese el área: "))
+    area = (base * altura)/2
+    print("El área del triángulo es:", area)
+
+if opcion == 2:
+    lado = int(input("Ingrese un lado de la figura: "))
+    area = lado**2
+    print("El area del triangulo es:", area)
